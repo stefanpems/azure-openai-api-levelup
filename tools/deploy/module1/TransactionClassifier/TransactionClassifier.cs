490ed4a8b4ccdf6cc43632a656a3633eedb53ebf
Stefano Pescosolido <58512303+stefanpems@users.noreply.github.com>
2023-05-16 12:45:55 +0000

    Test

--- tools/deploy/module1/TransactionClassifier/TransactionClassifier.cs
+++ tools/deploy/module1/TransactionClassifier/TransactionClassifier.cs
@@ -34,9 +34,9 @@ namespace TransactionClassification
         private const string prompt = @"You are an expert on cybersecurity, selling Microsoft security technologies. 
             You are answering a list of questions made by a customer interested mainly to Microsoft Defender for Cloud Apps.
             The customer is evaluating it against other solutions from competitors.
-            Ideally, you want to answer positively to every question. 
+            Ideally, you want to answer positively to every question.
             If not possible, you want to offer alternatives with the integration with other Microsoft security products.
-            Questions and answers are in Italian.
+            Questions and answers are in Italian. 
 
             Question: CUSTOMER_QUESTION
 
@@ -45,6 +45,8 @@ namespace TransactionClassification
         [FunctionName("TransactionClassifier")]
         public static void Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
         {
+            log.LogInformation("-----------------------------\n\n  Start execution - Run!\n-----------------------------");
+            
             // Get the blob information from the event grid event.
             var data = ((JObject)eventGridEvent.Data).ToObject<StorageBlobCreatedEventData>();
             // retrieve continer name from URL field ("https://egblobstore.blob.core.windows.net/{containername}/blobname.jpg") of the event grid event
@@ -67,6 +69,7 @@ namespace TransactionClassification
             OpenAIClient client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
 
             // read content of uploaded csv file
+            log.LogInformation("-----------------------------\n\n  Run - About to get records from CSV!\n-----------------------------");
             using var reader = new StreamReader(stream);
             using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
             var records = csv.GetRecords<dynamic>().ToList();
@@ -75,6 +78,7 @@ namespace TransactionClassification
 
             // for each csv record, set value in the column named "classification" to response from Azure OpenAI API Completion API
             // for each csv record, set value in the column named "Answer" to response from Azure OpenAI API Completion API
+            log.LogInformation("-----------------------------\n\n  Run - About to loop the input rows!\n-----------------------------");
             foreach (var record in records)
             {
                 /*
@@ -83,12 +87,13 @@ namespace TransactionClassification
                 record.classification = classification;
                 */
 
-                string answer = ClassifyTransaction(record, prompt, client);
+                string answer = ClassifyTransaction(record, prompt, client, log);
                 
                 record.Answer = answer;
             }
             
             // create a new file to store updated csv file
+            log.LogInformation("-----------------------------\n\n  Run - About to write the outputs!\n-----------------------------");
             using (var writer = new StringWriter())
             {
                 using (var acsv = new CsvWriter(writer, CultureInfo.InvariantCulture))
@@ -99,6 +104,7 @@ namespace TransactionClassification
                 }
 
                 // convert StringWriter to a Stream so it could be uploaded via BlobClient
+                log.LogInformation("-----------------------------\n\n  Run - About to upload the output file!\n-----------------------------");
                 var stream2 = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(writer.ToString()));
                 var outputBlobClient = new Azure.Storage.Blobs.BlobClient(connectionString, "output", blobName);
                 outputBlobClient.Upload(stream2);
@@ -106,7 +112,7 @@ namespace TransactionClassification
             
         }
                 
-        private static string ClassifyTransaction(dynamic transaction, string prompt, OpenAIClient client)
+        private static string ClassifyTransaction(dynamic transaction, string prompt, OpenAIClient client, ILogger log)
         {
             /*
             prompt = prompt.Replace("SUPPLIER_NAME", transaction.Supplier);
@@ -114,14 +120,22 @@ namespace TransactionClassification
             prompt = prompt.Replace("TRANSACTION_VALUE", transaction.Transaction_value);
             */
 
+            log.LogInformation("-----------------------------\n\n  ClassifyTransaction - prompt!\n-----------------------------");
             prompt = prompt.Replace("CUSTOMER_QUESTION", transaction.Question);
 
             string engine = GetEnvironmentVariable("OPENAI_API_MODEL");
 
             // call the Azure OpenAI API Completion API
-            Response<Completions> completionsResponse = client.GetCompletions(engine, prompt);
-            string completion = completionsResponse.Value.Choices[0].Text;
-
+            string completion = string.Empty;
+            try{
+                Response<Completions> completionsResponse = client.GetCompletions(engine, prompt);
+                completion = completionsResponse.Value.Choices[0].Text;
+            }
+            catch(Exception ex){
+                log.LogError("-----------------------------\n\n  ClassifyTransaction - Error!\n" + ex.ToString() + "\n-----------------------------");
+                throw(ex);
+            }
+            
             return completion;
         }
 
