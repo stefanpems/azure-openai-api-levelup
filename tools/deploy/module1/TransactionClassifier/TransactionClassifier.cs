22179e334da12b712c7751b6bf1570809748fcc2
Stefano Pescosolido <58512303+stefanpems@users.noreply.github.com>
2023-05-16 19:19:54 +0000

    Multiple out columns

--- tools/deploy/module1/TransactionClassifier/TransactionClassifier.cs
+++ tools/deploy/module1/TransactionClassifier/TransactionClassifier.cs
@@ -19,16 +19,30 @@ namespace TransactionClassification
     public static class TransactionClassifier
     {
 
-        private const string prompt = @"Sei un esperto di Microsoft Defender for Cloud Apps.
+        private const string promptSA = @"Sei un esperto di Microsoft Defender for Cloud Apps.
             Devi rispondere alla domanda di un cliente che chiede se il prodotto offre determinate capacità.
-            Laddove la risposta non fosse completamente positiva, proponi soluzioni con l'integrazione di altri prodotti Microsoft. 
-            Tieni un tono ufficiale ed impersonale, come per rispondere ad una domanda di una RFI.
-            Scrivi due righe per giustificare la risposta. 
-            Dove possibile, aggiungi link alla documentazione del prodotto in cui trovare dettagli su quanto affermi.
+            Le risposte possibili sono: Sì, No, Parzialmente.
 
             Domanda del cliente: CUSTOMER_QUESTION
 
             La risposta è: ";
+
+        private const string promptLA = @"Sei un esperto di Microsoft Defender for Cloud Apps.
+            Un cliente che chiede se il prodotto offre determinate capacità.
+            Rispondi in due righe, dando un minimo di spiegazioni.
+            Tieni un tono ufficiale ed impersonale.
+                         
+            Domanda del cliente: CUSTOMER_QUESTION
+
+            La risposta è: ";
+
+        private const string promptRL = @"Sei un esperto di Microsoft Defender for Cloud Apps.
+            Un cliente che chiede se il prodotto offre determinate capacità.
+            Segnala la URL della documentazione ufficiale Microsoft dove il cliente può trovare la risposta.
+
+            Domanda del cliente: CUSTOMER_QUESTION
+
+            La URL dove trovare la risposta è: ";
 
         [FunctionName("TransactionClassifier")]
         public static void Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
@@ -38,25 +52,25 @@ namespace TransactionClassification
             // Get the blob information from the event grid event.
             var data = ((JObject)eventGridEvent.Data).ToObject<StorageBlobCreatedEventData>();
             // retrieve continer name from URL field ("https://egblobstore.blob.core.windows.net/{containername}/blobname.jpg") of the event grid event
-            log.LogInformation("-----------------------------\n\n  Run - Debug #1\n-----------------------------");
+            log.LogInformation("----\n\n  Run - Debug #1\n-----------------------------");
             var containerName = data.Url.Split('/')[3];
-            log.LogInformation("-----------------------------\n\n  Run - Debug #2: " + containerName + "\n-----------------------------");
+            log.LogInformation("----\n\n  Run - Debug #2: " + containerName + "\n-----------------------------");
             // retrieve blob name from URL field ("https://egblobstore.blob.core.windows.net/containername/{blobname}.jpg") of the event grid event
             var blobName = data.Url.Split('/')[4]; 
-            log.LogInformation("-----------------------------\n\n  Run - Debug #3: " + blobName + "\n-----------------------------");
+            log.LogInformation("----\n\n  Run - Debug #3: " + blobName + "\n-----------------------------");
             
             var connectionString = GetEnvironmentVariable("STORAGE_ACCOUNT_CONNECTION_STRING");
-            log.LogInformation("-----------------------------\n\n  Run - Debug #4: " + connectionString + "\n-----------------------------");
+            log.LogInformation("----\n\n  Run - Debug #4: " + connectionString + "\n-----------------------------");
             
                 
             // Retrieve the blob from the storage account.
             var blobClient = new Azure.Storage.Blobs.BlobClient(connectionString, containerName, blobName);
             var blobContent = blobClient.DownloadContent();
-            log.LogInformation("-----------------------------\n\n  Run - Debug #5\n-----------------------------");
+            log.LogInformation("----\n\n  Run - Debug #5\n-----------------------------");
             
-            // convert from system.binary to system.io.stream
-            var stream = new MemoryStream(blobContent.Value.Content.ToArray());
-            log.LogInformation("-----------------------------\n\n  Run - Debug #6\n-----------------------------");
+            // convert from system.binary to system.io.streamIn
+            var streamIn = new MemoryStream(blobContent.Value.Content.ToArray());
+            log.LogInformation("----\n\n  Run - Debug #6\n-----------------------------");
             
 
             // set up connection to Azure OpenAI API client
@@ -66,29 +80,31 @@ namespace TransactionClassification
 
             // read content of uploaded csv file
             log.LogInformation("-----------------------------\n\n  Run - About to get records from CSV!\n-----------------------------");
-            using var reader = new StreamReader(stream);
+            using var reader = new StreamReader(streamIn);
             using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
             var records = csv.GetRecords<dynamic>().ToList();
 
-            string prompt = TransactionClassifier.prompt;
+            string promptSA = TransactionClassifier.promptSA;
+            string promptLA = TransactionClassifier.promptLA;
+            string promptRL = TransactionClassifier.promptRL;
 
-            // for each csv record, set value in the column named "classification" to response from Azure OpenAI API Completion API
-            // for each csv record, set value in the column named "Answer" to response from Azure OpenAI API Completion API
+
+            // for each csv record, set value in the columns named "ShortAnswer", "LongAnswer" and "ReferenceLink" with the responses from Azure OpenAI API Completion API
             log.LogInformation("-----------------------------\n\n  Run - About to loop the input rows!\n-----------------------------");
             int rn=0;
             foreach (var record in records)
             {
-                /*
-                string classification = ClassifyTransaction(record, prompt, client);
-                
-                record.classification = classification;
-                */
                 rn++;
                 log.LogInformation("-----------------------------\n\n  Run - row: " +rn.ToString() + " \n-----------------------------");
 
-                string answer = ClassifyTransaction(record, prompt, client, log);
+                string shortAnswer = QueryAOAI(record, promptSA, client, log);
+                record.ShortAnswer = shortAnswer;
                 
-                record.Answer = answer;
+                string longAnswer = QueryAOAI(record, promptLA, client, log);
+                record.LongAnswer = longAnswer;
+
+                string referenceLink = QueryAOAI(record, promptRL, client, log);
+                record.ReferenceLink = referenceLink;
             }
             
             // create a new file to store updated csv file
@@ -104,10 +120,10 @@ namespace TransactionClassification
 
                 // convert StringWriter to a Stream so it could be uploaded via BlobClient
                 log.LogInformation("-----------------------------\n\n  Run - About to upload the output file!\n-----------------------------");
-                var stream2 = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(writer.ToString()));
+                var streamOut = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(writer.ToString()));
                 var outBlobName = blobName.Replace("Q-","A-");
                 var outputBlobClient = new Azure.Storage.Blobs.BlobClient(connectionString, "output", outBlobName);
-                outputBlobClient.Upload(stream2);
+                outputBlobClient.Upload(streamOut);
             }
             
 
@@ -115,17 +131,11 @@ namespace TransactionClassification
                 
         }
                 
-        private static string ClassifyTransaction(dynamic transaction, string prompt, OpenAIClient client, ILogger log)
+        private static string QueryAOAI(dynamic transaction, string prompt, OpenAIClient client, ILogger log)
         {
-            /*
-            prompt = prompt.Replace("SUPPLIER_NAME", transaction.Supplier);
-            prompt = prompt.Replace("DESCRIPTION_TEXT", transaction.Description);
-            prompt = prompt.Replace("TRANSACTION_VALUE", transaction.Transaction_value);
-            */
-            
             prompt = prompt.Replace("CUSTOMER_QUESTION", transaction.Question);
 
-            log.LogInformation("---------------------------------------\n\n  ClassifyTransaction - Prompt:\n---------------------------------------");
+            log.LogInformation("---------------------------------------\n\n  QueryAOAI - Prompt:\n---------------------------------------");
             log.LogInformation(prompt);
             log.LogInformation("---------------------------------------");
             
@@ -136,17 +146,18 @@ namespace TransactionClassification
             try{
                 Response<Completions> completionsResponse = client.GetCompletions(engine, prompt);
                 completion = completionsResponse.Value.Choices[0].Text;
-                log.LogInformation("-------------------------------------------------\n\n  ClassifyTransaction - Completion:\n-------------------------------------------------");
-                log.LogInformation(completion);
-                log.LogInformation("-------------------------------------------------");
             }
             catch(Exception ex){
-                log.LogError("-----------------------------\n\n  ClassifyTransaction - Error! \n" + ex.ToString() + "\n-----------------------------");
+                log.LogError("-----------------------------\n\n  QueryAOAI - Error! \n" + ex.ToString() + "\n-----------------------------");
                 completion = string.Concat("ERROR: ", ex.Message);
-                log.LogInformation("-----------------------------");
+                log.LogError("-----------------------------");
                 //do not throw(ex);
             }
-            
+
+            log.LogInformation("-------------------------------------------------\n\n  QueryAOAI - Completion:\n-------------------------------------------------");
+            log.LogInformation(completion);
+            log.LogInformation("-------------------------------------------------");   
+                     
             return completion;
         }
 
